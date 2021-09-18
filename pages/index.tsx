29259d60e8a08d564815a9cb517d7c7c17be685f
listentothefrog <shashanke7y@gmail.com>
2021-09-18 19:21:19 -0400

    added navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,24 @@ import Head from "next/head";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col">
       <Head>
         <title>Shashank Ellareddy - Coder, Gamer and Football Lover</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col justify-center bg-white">
+      <nav className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8 bg-opacity-60">
+        <h2 className="text-sm md:text-lg font-bold">Shashank</h2>
+        <div>
+          <a className="p-1 text-gray-900 sm:p-4">Stats</a>
+          <a className="p-1 text-gray-900 sm:p-4">About Me</a>
+          <a className="p-1 text-gray-900 sm:p-4">Blogs</a>
+          <a className="p-1 text-gray-900 sm:p-4" href="/">
+            Home
+          </a>
+        </div>
+      </nav>
+      <main className="flex flex-col justify-center bg-white px-2">
         <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
           <div>
             <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black">
