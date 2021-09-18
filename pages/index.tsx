bc537c4323dbe177186d8912b8be477b89f0915a
listentothefrog <shashanke7y@gmail.com>
2021-09-18 18:44:15 -0400

    about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,52 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
-        <title>Create Next App</title>
+        <title>Shashank Ellareddy - Coder, Gamer and Football Lover</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+      <main className="flex flex-col justify-center bg-white">
+        <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
+          <div>
+            <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black">
+              Hello 👋
+            </h1>
+            <p className="prose text-gray-600 dark:text-gray-400 mb-16">
+              My name is Shashank, I am freshman at{" "}
+              <a
+                className="text-blue-600 hover:text-blue-500"
+                href="https://www.ucps.k12.nc.us/Domain/11"
+                target="_blank"
+              >
+                Cuthburtson High
+              </a>
+              {""} and was introduced to coding when I was in 7th Grade. I am
+              really happy that you are willing to read more about me and who I
+              am. While you are here checkout my {""}
+              <a
+                className="text-blue-600 hover:text-blue-500"
+                href=""
+                target="_blank"
+              >
+                projects
+              </a>
+              {""} and {""}
+              <a
+                className="text-blue-600 hover:text-blue-500"
+                href=""
+                target="_blank"
+              >
+                learn more about me.
+              </a>
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </div>
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
