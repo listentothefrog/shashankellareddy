df0516f100af1a10d4151ed1a175c5b815334eae
listentothefrog <shashanke7y@gmail.com>
2021-09-18 23:25:45 -0400

    sticky navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <nav className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8 bg-opacity-60">
+    <nav className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white sticky md:my-8 bg-opacity-60">
       <h2 className="text-sm md:text-lg font-bold">Shashank</h2>
       <div>
         <a className="p-1 text-gray-900 sm:p-4">Stats</a>
