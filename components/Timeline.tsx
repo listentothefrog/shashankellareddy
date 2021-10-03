a0de924b0e7b6bb7cb0af7423c5452caa24aa613
listentothefrog <shashanke7y@gmail.com>
2021-10-03 15:06:09 -0400

    replaced checkmark with circle

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -17,19 +17,17 @@ const TimelineTitle = (props: any) => {
     <li className="mb-4 ml-2">
       <div className="flex items-center mb-2 text-green-700 dark:text-green-300">
         <span className="sr-only">Check</span>
-        <svg className="h-4 w-4 mr-2" viewBox="0 0 24 24">
-          <g
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M22 11.08V12a10 10 0 11-5.93-9.14" />
-            <path d="M22 4L12 14.01l-3-3" />
-          </g>
+        <svg
+          width="12"
+          height="12"
+          viewBox="0 0 12 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="6" cy="6" r="5.5" stroke="#47C87D" />
         </svg>
-        <p className="font-medium text-gray-900 dark:text-gray-100">
+
+        <p className="font-medium text-gray-900 dark:text-gray-100 ml-3">
           {props.title}
         </p>
       </div>
